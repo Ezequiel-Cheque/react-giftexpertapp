1dd9512ad7a4e7f870f4d516ddd8d142ae3ca4ff
Ezequiel <delaluzezequiel@gmail.com>
2020-09-11 14:08:22 -0500

    Second commit

---
+++ docs/precache-manifest.9b9420202e4480c0a98ca330e1ee7f0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50b29878cfefeb7c723e04b2bea00fe7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c5ec5ef64af811372a7",
+    "url": "/static/css/main.c3fb7544.chunk.css"
+  },
+  {
+    "revision": "26227e71ad7d4f1513b6",
+    "url": "/static/js/2.d7cf9fae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d7cf9fae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c5ec5ef64af811372a7",
+    "url": "/static/js/main.74e90010.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
